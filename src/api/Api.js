7726167325a60dcac7b8dc23ed9798efb7d17362
kodirov8788 <kodirov8788@gmail.com>
2023-09-01 00:44:55 +0500

    dfsf

--- src/api/Api.js
+++ src/api/Api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const Axios = axios.create({
-    // baseURL: 'https://cafe-app-s03x.onrender.com/'
-    baseURL: 'http://localhost:8000'
+    baseURL: 'https://cafe-app-s03x.onrender.com/'
+    // baseURL: 'http://localhost:8000'
 });
 
 export default Axios
